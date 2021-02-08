b0c04efd51341c8865aaf6372aee3498192de55f
jernest1-spec <69368718+jernest1-spec@users.noreply.github.com>
2021-02-08 10:14:49 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ex-2-Dig4639
+
+"Even the greatest was once a beginner. Don't be afraid to take that first step." ~ Muhammad Ali
---
+++ index.js
@@ -0,0 +1,7 @@
+let quotes = [`"Even the greatest was once a beginner. Don't be afraid to take that first step." ~ Muhammad Ali`,
+`"An artist, if he is truly an artist, is only interested in one thing and that is to wake up the minds of men, to have mankind and womankind realize that there is something greater than what we see on the surface." ~ Marvin Gaye`,
+`"It's been a long time coming. But I know a change is gonna come" ~ Sam Cooke`
+]
+
+let index = Math.floor(Math.random() * 3)
+console.log(quotes[index])
